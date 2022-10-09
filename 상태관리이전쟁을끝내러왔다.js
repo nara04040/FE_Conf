24f6c5d0713a0f08bf8ecdab890c3e9d700d747c
nara04040 <nara04040@naver.com>
2022-10-09 18:33:18 +0900

    강연 정리

---
+++ 상태관리이전쟁을끝내러왔다.js
@@ -0,0 +1,21 @@
+/**
+ * 아키텍처 문제
+ * 관심사의 분리
+ *
+ * Ui , Business Logic의 분리
+ *
+ * redux
+ * 단점
+ * 1. Plain Object(순수함수)
+ *  Class를 쓰여야한다. => 객체지향의 접근방식에 익숙
+ *
+ * 2. 거대 단일 스토어
+ *   Micre Stroe => recoil, zustand ... 상태관리
+ *  그래서 어떻게 만들것인가?
+ *
+ * External Store ㄹ
+ *
+ * useSyncExternalStore _> react의 hook
+ *
+ *
+ */
